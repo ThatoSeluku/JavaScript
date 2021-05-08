9b482a962852dcfe740884570997ea1165043c1e
ThatoSeluku <Thato1000seluku@gmail.com>
2021-05-08 20:57:05 +0200

    Completed modal app using Javascript

--- 6-modal/setup/app.js
+++ 6-modal/setup/app.js
@@ -1,4 +1,16 @@
 // select modal-btn,modal-overlay,close-btn
+const modalbtn = document.querySelector(".modal-btn");
+const modaloverlay = document.querySelector(".modal-overlay");
+const closebtn = document.querySelector(".close-btn");
 // listen for click events on modal-btn and close-btn
+modalbtn.addEventListener("click", function(){
+modaloverlay.classList.add("open-modal");
+});
+
+closebtn.addEventListener("click", function(){
+modaloverlay.classList.remove("open-modal");
+});
+
 // when user clicks modal-btn add .open-modal to modal-overlay
+
 // when user clicks close-btn remove .open-modal from modal-overlay
