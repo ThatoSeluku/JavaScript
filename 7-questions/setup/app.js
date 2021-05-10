0ba29fb5e9e7444695b2b25b6a45866dc1b20dc9
ThatoSeluku <Thato1000seluku@gmail.com>
2021-05-10 20:31:03 +0200

    Completed Questions and answer HTML

--- 7-questions/setup/app.js
+++ 7-questions/setup/app.js
@@ -1,2 +1,11 @@
 //using selectors inside the element
+//Select 'question' query
+const questions = document.querySelectorAll(".question"); 
+//Select each instance of "question"
+questions.forEach(function (question) {
+ const btn = question.querySelector(".question-btn");
+
+
+  });
+
 // traversing the dom
