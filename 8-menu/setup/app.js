b61d508891a85e17398aebbe52e818dba8d740d4
ThatoSeluku <Thato1000seluku@gmail.com>
2021-05-11 19:00:43 +0200

    Used innerHTML method to display the entire string, making it available to view as an HTML page

--- 8-menu/setup/app.js
+++ 8-menu/setup/app.js
@@ -91,8 +91,21 @@ console.log("While execution");
 window.addEventListener("DOMContentLoaded", function(){
 let displayMenu = menu.map(function(item){
 //Return title of each item
-  return `<h1>${item.title}</h1>`;
+  return `<article class="menu-item">
+<img src=${item.img} class="photo"/>
+<div class="item-info">
+ <header>
+  <h4>${item.title}</h4>
+  <h4 class="price">${"$"+item.price}</h4>
+</header>
+<p class="item-text">${item.desc}</p>
+</div>
+</article>`;
+  
 });
-console.log(displayMenu);
+//Iteration complete: Now take all those items, and join into one string
+displayMenu = displayMenu.join("");
+//Take that string and append it on HTML code
+sectionCenter.innerHTML = displayMenu
 });
 
