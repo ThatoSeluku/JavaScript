d7b585712549584d5e9d378dd393a16c4fd626b3
ThatoSeluku <Thato1000seluku@gmail.com>
2021-05-24 19:50:21 +0200

    checkforMatch and createBoard function are complete

--- memorygame/memory-game/app.js
+++ memorygame/memory-game/app.js
@@ -54,5 +54,51 @@ img: 'images/fries.png'
 
 ]
 
+//Select 'grid' query
+const grid = document.querySelector('.grid')
 
-})
+var cardsChosen = []
+var cardsChosenId =[]
+
+//Function that creates the game board
+function createBoard(){
+for(let i=0; i<cardArray.length; i++)
+//Create card element and set it to img:
+var card = document.createElement('img')
+//Set all images to white image, this acts as the blank canvas
+card.setAttribute('src', 'images/blank.png')
+//Set id to each element(i) of card
+card.setAttribute('data-id', i)
+//Call flipcard function when image is clicked
+card.addEventListener('click', flipcard)
+grid.appendChild(card)
+}
+},
+
+//Check for matches
+function checkForMatch(){
+  //Get all cards and put them in a query selector:
+  var cards = document.querySelectorAll('img')
+  //Get first value of array and assign to 
+  const optionOneId = cardsChosenId[0]
+
+}
+
+
+//Flip the card
+function flipcard(){
+ //Select the cardId
+  var cardId = this.getAttribute('data-id')
+//push chosen card to top of array list by name
+cardChosen.push(cardArray[cardId].name)
+cardsChosenId .push(cardId)
+ //set image attribute to src and image
+this.setAttribute('src', cardArray[cardId].img)
+//Check for match after half a second
+if (cardsChosen.length ===2){
+  setTimeout(checkForMatch, 500)
+}
+}
+
+
+)
