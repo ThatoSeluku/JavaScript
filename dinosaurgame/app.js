83d60bf3b65b2a16014c1f479a58f263f7e64d05
ThatoSeluku <Thato1000seluku@gmail.com>
2021-05-25 21:35:38 +0200

    Done JS

--- dinosaurgame/app.js
+++ dinosaurgame/app.js
@@ -8,3 +8,18 @@ function jump(){
 character.classList.remove("animate");
     }, 500);
 }
+//Create function that checks if you're dead every 10ms
+var checkDeath = setInterval(function(){
+
+var characterTop =   
+parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+
+var blockLeft =   
+parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+if(blockLeft<20&&blockLeft>20&&characterTop>=130)
+{
+    block.style.animation = "none";
+    block.style.display = "none";
+    alert("You lost");
+}
+}, 10);
